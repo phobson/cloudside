bd074282808ff4896de34f4b861870bbcea15775
Paul Hobson <pmhobson@gmail.com>
2019-11-07 09:04:20 -0800

    add CLI options for outfile

--- cloudside/cli.py
+++ cloudside/cli.py
@@ -15,22 +15,35 @@ def main():
 
 
 @main.command()
-@click.argument('station')
-@click.argument('startdate')
-@click.argument('enddate')
-@click.argument('email')
-@click.option('--folder')
-@click.option('--force', is_flag=True)
-def get_asos(station, startdate, enddate, email, folder, force):
-    folder = '.' or folder
-    return asos.get_data(station, startdate, enddate, email, folder=folder,
-                         force_download=force, pbar_fxn=tqdm)
+@click.argument("station")
+@click.argument("startdate")
+@click.argument("enddate")
+@click.argument("email")
+@click.option("--folder")
+@click.option("--force", is_flag=True)
+@click.option("--outfile")
+def get_asos(station, startdate, enddate, email, folder, force, outfile):
+    folder = "." or folder
+    df = asos.get_data(
+        station,
+        startdate,
+        enddate,
+        email,
+        folder=folder,
+        force_download=force,
+        pbar_fxn=tqdm,
+    )
+    if outfile:
+        df.to_csv(outfile, encoding="utf-8")
 
 
 @main.command()
-@click.argument('station')
-@click.option('--folder')
-@click.option('--force', is_flag=True)
-def get_hydra(station, folder, force):
-    folder = '.' or folder
-    return hydra.get_data(station, folder=folder, force_download=force)
+@click.argument("station")
+@click.option("--folder")
+@click.option("--force", is_flag=True)
+@click.option("--outfile")
+def get_hydra(station, folder, force, outfile):
+    folder = "." or folder
+    df = hydra.get_data(station, folder=folder, force_download=force)
+    if outfile:
+        df.to_csv(outfile, encoding="utf-8")
